6aa06335922486852c9c116b5542a634d14b2289
usftt <usftt@student.kit.edu>
2024-04-26 17:24:47 +0200

    autocorrelation

--- scripts/train_gan.py
+++ scripts/train_gan.py
@@ -1,20 +1,21 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-
 import numpy as np
-import matplotlib.pyplot as plt
 
 from utils import auto_corr
 from gan import Generator, Discriminator
 
 
-BATCH_SIZE = 256
+REGIME = 0
+BATCH_SIZE_G = 256
+BATCH_SIZE_D = 512
 MINIBATCH_SIZE = 32
 SUB_EPOCHS = [(3, 3)] * 50
 LEARNING_RATE_G = 1e-5
 LEARNING_RATE_D = 1e-5
 NOISE_DIM = 100
+STAT_LOSS_WEIGHT = 0.1
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -23,99 +24,97 @@ def load_data():
     X = torch.tensor(X, dtype=torch.float32)
     y = np.load('../data/y.npy')
     y = torch.tensor(y, dtype=torch.float32)
-    X = X[y == 0]
+    X = X[y == REGIME]
     X = (X - X.min()) / (X.max() - X.min())
     X = X.reshape(-1, 1, 1, 96)
     return X
+
+def stat_loss(fake_data, X):
+    fake_data = fake_data.reshape(-1, 1, 9, 96)
+    fake_data_diff = fake_data[:, :, :, 1:] - fake_data[:, :, :, :-1]
+    X_diff = X[:, :, :, 1:] - X[:, :, :, :-1]
+    return sum([
+        (fake_data.mean() - X.mean())**2,
+        (fake_data_diff.mean() - X_diff.mean())**2,
+        (fake_data.std() - X.std())**2,
+        (fake_data_diff.std() - X_diff.std())**2,
+    ] + [
+        (3/4)**i * (auto_corr(fake_data, i).mean() - auto_corr(X, i).mean())**2 for i in range(10)
+    ] + [
+        (3/4)**i * (auto_corr(fake_data_diff, i).mean() - auto_corr(X_diff, i).mean())**2 for i in range(10)
+    ])
+
+def reset_model(model):
+    for layer in model.children():
+        if hasattr(layer, 'reset_parameters'):
+            layer.reset_parameters()
 
 def train_discriminator(optimizer, model, criterion, X):
     optimizer.zero_grad()
 
     indices = np.random.choice(X.shape[0], MINIBATCH_SIZE * 9, replace=False)
     real_data = X[indices].reshape(-1, 1, 9, 96).to(device)
-    real_data = real_data + 0.01 * torch.randn_like(real_data, device=device)
-    fake_data = G(torch.randn(MINIBATCH_SIZE, NOISE_DIM, 1, 1, device=device))[:,:,:,25:-25]
-
+    real_data = real_data + 0.001 * torch.randn_like(real_data, device=device)
     D_real = model(real_data)
     D_real_loss = criterion(D_real, torch.ones_like(D_real))
     
+    fake_data = G(torch.randn(MINIBATCH_SIZE, NOISE_DIM, 1, 1, device=device))[:,:,:,25:-25]
     D_fake = model(fake_data.detach())
     D_fake_loss = criterion(D_fake, torch.zeros_like(D_fake))
 
     D_loss = D_real_loss + D_fake_loss
     D_loss.backward()
     optimizer.step()
-    return D_loss
+    return D_real_loss, D_fake_loss
 
-def train_generator(optimizer, discriminator, generator, criterion, auto_corrs):
+def train_generator(optimizer, discriminator, generator, criterion, X):
     optimizer.zero_grad()
 
     fake_data = generator(torch.randn(MINIBATCH_SIZE, NOISE_DIM, 1, 1, device=device))[:,:,:,25:-25]
-    auto_corrs_fake = [auto_corr(fake_data, i + 1).mean() for i in range(len(auto_corrs))]
-    G_stat_loss = sum(torch.abs(auto_corrs_fake[i] - auto_corrs[i]) for i in range(len(auto_corrs)))
+    G_stat_loss = stat_loss(fake_data, X)
 
     output = discriminator(fake_data)
     G_disc_loss = criterion(output, torch.ones_like(output))
 
-    G_loss = G_disc_loss + 0.1 * G_stat_loss
+    G_loss = G_disc_loss + STAT_LOSS_WEIGHT * G_stat_loss
     G_loss.backward()
     optimizer.step()
-    return G_loss, auto_corrs_fake
+    return G_stat_loss, G_disc_loss
 
 def train(n_sub_epochs, num_epochs, X):
     epoch = 0
     best_score = float('inf')
-    auto_corrs = [auto_corr(X, i).mean() for i in range(1, 4)]
     for sub_epoch in n_sub_epochs:
+        reset_model(D)
         for _ in range(sub_epoch[0]):
-            for _ in range(BATCH_SIZE):
-                D_loss = train_discriminator(optimizer_D, D, criterion, X)
             epoch += 1
-            print(f"Epoch {epoch}/{num_epochs}, D Loss: {D_loss.item()}")
+            for _ in range(BATCH_SIZE_D):
+                D_real_loss, D_fake_loss = train_discriminator(optimizer_D, D, criterion, X)
+            print(f"Epoch {epoch}/{num_epochs}, D Real Loss: {D_real_loss.item()}, D Fake Loss: {D_fake_loss.item()}")
 
         for _ in range(sub_epoch[1]):
-            for _ in range(BATCH_SIZE):
-                G_loss, auto_corrs_fake = train_generator(optimizer_G, D, G, criterion, auto_corrs)
             epoch += 1
-            print(f"Epoch {epoch}/{num_epochs}, G Loss: {G_loss.item()}")
+            for _ in range(BATCH_SIZE_G):
+                G_stat_loss, G_disc_loss = train_generator(optimizer_G, D, G, criterion, X)
+            print(f"Epoch {epoch}/{num_epochs}, G Stat Loss: {G_stat_loss.item()}, G Disc Loss: {G_disc_loss.item()}")
 
-        print(f"Auto-correlations real: {auto_corrs}")
-        print(f"Auto-correlations fake: {auto_corrs_fake}")
-        score = sum(torch.abs(auto_corrs_fake[i] - auto_corrs[i]) for i in range(len(auto_corrs)))
-        if score < best_score:
-            best_score = score
+        if G_stat_loss < best_score:
+            best_score = G_stat_loss
             best_G = G
-            print(f"Best score: {score}")
+            print(f"Best score: {G_stat_loss}")
+            torch.save(best_G.state_dict(), f'../models/best_generator_regime{REGIME}.pth')
 
     return best_G
 
 if __name__ == '__main__':
-
     X = load_data()
-    G = Generator(NOISE_DIM).to(device)
     D = Discriminator().to(device)
+    G = Generator(NOISE_DIM).to(device)
 
     criterion = nn.BCELoss()
     optimizer_G = optim.Adam(G.parameters(), lr=LEARNING_RATE_G)
     optimizer_D = optim.Adam(D.parameters(), lr=LEARNING_RATE_D)
 
     num_epochs = sum(sum(sub_epoch) for sub_epoch in SUB_EPOCHS)
-
-    best_G = train(SUB_EPOCHS, num_epochs, X)
-
-    torch.save(best_G.state_dict(), '../models/best_generator.pth')
-    torch.save(G.state_dict(), '../models/final_generator.pth')
-
-    best_G.eval()
-    fake_data = best_G(torch.randn(2, NOISE_DIM, 1, 1, device=device))[:,:,:,25:-25].reshape(-1, 96).cpu()
-    for i in range(12):
-        plt.plot(fake_data[i].detach().numpy(), color='red')
-        plt.plot(X[i, 0].detach().numpy(), color='blue')
-    plt.savefig('../images/best_generator.png')
-
-    G.eval()
-    fake_data = G(torch.randn(2, NOISE_DIM, 1, 1, device=device))[:,:,:,25:-25].reshape(-1, 96).cpu()
-    for i in range(12):
-        plt.plot(fake_data[i].detach().numpy(), color='red')
-        plt.plot(X[i, 0].detach().numpy(), color='blue')
-    plt.savefig('../images/final_generator.png')
+    train(SUB_EPOCHS, num_epochs, X)
+    torch.save(G.state_dict(), f'../models/final_generator_regime{REGIME}.pth')
